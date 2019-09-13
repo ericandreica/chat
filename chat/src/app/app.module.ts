4ad0df7c120e43be025e36d5f1fe5fcd0c579742
EAndreica <andreica_eric@yahoo.com>
2019-09-13 14:52:05 +0300

    Added navbar.

--- chat/src/app/app.module.ts
+++ chat/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule} from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ChatComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
